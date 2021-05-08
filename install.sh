e9ef92976dcd5f4677f2b5b65b51a34aa72ae681
Florian.D <floriandjerbi@free.fr>
2021-05-08 19:11:48 +0200

    Create user php7 and 8 and folder /www

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
+# Create user and folder /www
+create_user () {
+	sudo useradd -d /home/test-php7.vigicorp.net -s /bin/bash php7
+	sudo useradd -d /home/test-php8.vigicorp.net -s /bin/bash php8
+	[ $? -eq 0 ] && echo "User php7 and php8 has been added to system!"
+    sudo mkdir /home/test-php{7,8}.vigicorp.net/www
+}
+
 # Am i Root user?
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -p "Enter home directory : " username
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
-	else
-		useradd -d /home/"$directory" -s /bin/bash "$username"
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
+if [ $(id -u) -eq 0 ] || [ $(whoami) = "debian" ]; then
+    sudo apt update && sudo apt upgrade
+    create_user
 else
-	echo "Only root may add a user to the system."
+	echo "Only root or debian may add a user to the system."
 	exit 2
 fi

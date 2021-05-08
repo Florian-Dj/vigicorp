444bd409f1ba5096acde51b9c4854db19a8776d3
Florian.D <floriandjerbi@free.fr>
2021-05-08 20:29:54 +0200

    Rebuild create_user function

--- install.sh
+++ install.sh
@@ -2,17 +2,23 @@
 
 # Create user and folder /www
 create_user () {
-	sudo useradd -d /home/test-php7.vigicorp.net -s /bin/bash php7
-	sudo useradd -d /home/test-php8.vigicorp.net -s /bin/bash php8
-	[ $? -eq 0 ] && echo "User php7 and php8 has been added to system!"
-    sudo mkdir /home/test-php{7,8}.vigicorp.net/www
+	#User php7
+	egrep "^$1" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "User $1 exists!"
+	else
+	    adduser --home /home/test-"$1".vigicorp.net --shell /bin/bash --disabled-password $1
+		echo "User $1 has been added to system!" || echo "Failed to add a user!"
+	fi
 }
 
 # Am i Root user?
-if [ $(id -u) -eq 0 ] || [ $(whoami) = "debian" ]; then
-    sudo apt update && sudo apt upgrade
-    create_user
+if [ $(id -u) -eq 0 ]; then
+    apt update && apt upgrade -y
+    create_user 'php7'
+    create_user 'php8'
+    mkdir /home/test-php{7,8}.vigicorp.net/www
 else
-	echo "Only root or debian may add a user to the system."
+	echo "Only root may add a user to the system."
 	exit 2
 fi

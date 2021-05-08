eb7f63380d0df79a94c4317a1c977ad05980ccac
Florian.D <floriandjerbi@free.fr>
2021-05-08 21:57:57 +0200

    Create function install-apache and install_php

--- install.sh
+++ install.sh
@@ -14,11 +14,30 @@ create_user () {
 	fi
 }
 
+#Install apache2
+install_apache () {
+	apt -y install lsb-release apt-transport-https ca-certificates wget apache2
+	wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+	apt update -y
+	systemctl start apache2
+	systemctl enable apache2
+}
+
+#Install php librairies for apache2
+install_php () {
+	apt install -y $1 $1-cli $1-common
+	apt install -y libapache2-mod-$1
+}
+
 # Am i Root user?
 if [ $(id -u) -eq 0 ]; then
 	apt update && apt upgrade -y
 	create_user 'php7'
 	create_user 'php8'
+	install_apache
+	install_php 'php7.4'
+	install_php 'php8.0'
 else
 	echo "Only root may add a user to the system."
 	exit 2
